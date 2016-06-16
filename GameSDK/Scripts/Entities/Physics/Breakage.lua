55938ca24e2c4ecb74abc193232e95b5f8f18e0a
David Kaye <davidka@crytek.de>
2016-06-16 15:24:30 +0200

    !D (GameSDK) Remove GameSDK-specific data files. (Approved by thomasw) Review: ID 56730 by Author: David Kaye, Reviewer: Thomas Wollenzin, Observer: Scott Peter
    Copied from Perforce
     Change: 1403386

--- GameSDK/Scripts/Entities/Physics/Breakage.lua
+++
@@ -1,9 +0,0 @@
-Script.ReloadScript("scripts/Utils/EntityUtils.lua")
-
--- Breakage entity
-Breakage = {
-}
-
-function Breakage:IsUsable(user)
-	return 0;
-end
--- GameSDK/Scripts/GameRules/Gladiator.lua
+++
@@ -1,6 +0,0 @@
-Script.ReloadScript("scripts/gamerules/GameRulesUtils.lua");
-
-Gladiator = {};
-
-GameRulesSetStandardFuncs(Gladiator);
-
