f3ee05600ba885feff1625a56c440bc657c1b0c5
Tomas Oresky <tomaso@crytek.de>
2016-10-11 17:29:15 +0200

    !XB (Sandbox) (CE-10668) ENTITY: Not possible to change any parameters of any entity (Approved by samuelk) Review: ID 61608 by Author: Tomas Oresky, Reviewer: Filip Lundgren, Observer: Nico Moss, Observer: Samuel Kahn
    Copied from Perforce
     Change: 1453437

--- Code/CryEngine/CryCommon/CrySerialization/Decorators/Range.h
+++ Code/CryEngine/CryCommon/CrySerialization/Decorators/Range.h
@@ -2,46 +2,17 @@
 
 #pragma once
 
+#include <yasli/decorators/Range.h>
+
 namespace Serialization
 {
+	using yasli::RangeDecorator;
+	using yasli::Range;
 
-template<typename Type>
-struct DefaultSinglestep
-{
-	static double value()
+	namespace Decorators
 	{
-		return std::is_integral<Type>::value ? 1.0 : 0.1;
+		using yasli::RangeDecorator;
+		using yasli::Range;
 	}
-};
-
-template<class T>
-struct RangeDecorator
-{
-	T* value;
-	T hardMin;
-	T hardMax;
-	T singleStep;
-
-	void YASLI_SERIALIZE_METHOD(class yasli::Archive& ar) {}
-};
-
-template<class T>
-RangeDecorator<T> Range(T& value, T hardMin, T hardMax, T singleStep = (T)DefaultSinglestep<T>::value())
-{
-	RangeDecorator<T> r;
-	r.value = &value;
-	r.hardMin = hardMin;
-	r.hardMax = hardMax;
-	r.singleStep = singleStep;
-	return r;
 }
 
-namespace Decorators
-{
-//! Obsolete name, will be removed. Please use Serialization::Range instead.
-using Serialization::Range;
-}
-
-}
-
-#include "RangeImpl.h"
