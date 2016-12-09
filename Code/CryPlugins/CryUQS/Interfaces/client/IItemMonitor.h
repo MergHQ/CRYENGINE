7641a57136782d435ea9af4eb143e1e1d7f9149f
Christian Werle <christianw@crytek.de>
2016-12-09 15:35:02 +0100

    !B (DEV-3297) (UQS) client-side ItemMonitors would get destroyed by the wrong deleter on the core-side, thus causing crashes in mixed debug/release builds (Approved by thomasw) Review: ID 63664 by Author: Christian Werle, Reviewer: Marek Zeman, Observer: Sandy Brand, Observer: Marzena Jaworska, Observer: Pavlo Ilin, Observer: Calin Bindea
    Copied from Perforce
     Change: 1477454

--- Code/CryPlugins/CryUQS/Interfaces/client/IItemMonitor.h
+++ Code/CryPlugins/CryUQS/Interfaces/client/IItemMonitor.h
@@ -41,7 +41,28 @@ namespace uqs
 
 			virtual               ~IItemMonitor() {}
 			virtual EHealthState  UpdateAndCheckForCorruption(shared::IUqsString& outExplanationInCaseOfCorruption) = 0;
+
+			// All these innocently looking operator-new/delete functions do is call their global counterparts, but there's a specific reason for why we have them defined in this class:
+			// When the caller on the *client*-side instantiates an IItemMonitor, he will do so using the new-operator. Nothing fancy about that yet. Then, he will
+			// hand over that object to an ItemMonitorUniquePtr (which is a std::unique_ptr) and pass that to the *core*-side of things. From then on, the item-monitor
+			// lives on the *core*-side. Eventually, the ItemMonitorUniquePtr will go out of scope, and ask the std::unique_ptr's deleter to destroy the object. Things become
+			// interesting now: std::unique_ptr will invoke the C++ language's delete-operator, which in turn calls an operator-delete-function and that particular function is
+			// the delete-operator of this class. Now comes the important part: since this delete-operator-function is declared *inline* here, it's code will actually reside
+			// on the *client*-side again, which is exactly what we want :-)
+			// The operator-new-function is just here for completeness.
+			static void*          operator new(size_t numBytes);
+			static void           operator delete(void* p);
 		};
+
+		inline void* IItemMonitor::operator new(size_t numBytes)
+		{
+			return ::operator new(numBytes);
+		}
+
+		inline void IItemMonitor::operator delete(void* p)
+		{
+			::operator delete(p);
+		}
 
 	}
 }
