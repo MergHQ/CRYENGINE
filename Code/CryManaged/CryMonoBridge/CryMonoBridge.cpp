2fd195370c70c738666dc3ad08687f9c1579a8ba
Alexander Klinger <alexanderkl@crytek.de>
2017-02-06 15:13:40 +0100

    !B (CE-11169) (CrySystem) Sydewinder doesn't close properly in launcher (Approved by achim) Review: ID 64274 by Author: Alexander Klinger, Reviewer: Marco Hopp, Observer: Achim Lang, Observer: Jan Hellwig, Observer: Chan Tuck Mun
    Copied from Perforce
     Change: 1487774

--- Code/CryManaged/CryMonoBridge/CryMonoBridge.cpp
+++ Code/CryManaged/CryMonoBridge/CryMonoBridge.cpp
@@ -21,26 +21,6 @@ BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserve
 #endif // WIN32
 
 //////////////////////////////////////////////////////////////////////////
-class CSystemEventListener_CryMonoBridge : public ISystemEventListener
-{
-
-public:
-	virtual void OnSystemEvent(ESystemEvent event, UINT_PTR wparam, UINT_PTR lparam)
-	{
-		switch (event)
-		{
-		case ESYSTEM_EVENT_FULL_SHUTDOWN:
-		{
-			SAFE_DELETE(gEnv->pMonoRuntime);
-		}
-		break;
-		}
-	}
-};
-
-static CSystemEventListener_CryMonoBridge g_system_event_listener_crymonobridge;
-
-//////////////////////////////////////////////////////////////////////////
 class CEngineModule_CryMonoBridge : public IEngineModule
 {
 	CRYINTERFACE_SIMPLE(IEngineModule)
@@ -53,7 +33,6 @@ class CEngineModule_CryMonoBridge : public IEngineModule
 
 	virtual bool        Initialize(SSystemGlobalEnvironment& env, const SSystemInitParams& initParams)
 	{
-		env.pSystem->GetISystemEventDispatcher()->RegisterListener(&g_system_event_listener_crymonobridge);
 		env.pMonoRuntime = new CMonoRuntime();
 
 		return env.pMonoRuntime->Initialize();
