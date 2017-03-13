75ccfb325bf47f8f4514f54e3b0e6e38c1460eb5
Michael Madsen <michaelma@crytek.de>
2017-03-13 11:55:17 +0100

    !XT (CMake, MSVC) Disable warning C4653 to allow modifying optimization levels on individual files when using PCH (Approved by achim) !XT (CMake, MSVC) Create PDB and debug info for release builds Review: ID 64967 by Author: Michael Madsen, Reviewer: Nico Moss, Observer: Achim Lang, Observer: Timur Davidenko
    Copied from Perforce
     Change: 1497609

--- Tools/CMake/CRYENGINE-MSVC.cmake
+++ Tools/CMake/CRYENGINE-MSVC.cmake
@@ -1,18 +1,19 @@
 
 set(MSVC_COMMON_FLAGS 
-	/nologo		# Don't show version info
-	/W3 		# Enable warning level 3
-	/fp:fast 	# Use fast floating point precision model
-	/Zc:wchar_t	# Parse wchar_t as internal type
-	/GF 		# Eliminate Duplicate Strings
-	/Gy		# Enable function level linking
-	/GR- 		# Disable RTTI
-	/utf-8		# Set source and execution charset to utf-8
-	/Wv:18		# Disable warnings until SDK depedencies switch to UTF-8/ASCII.
-	/MP
-	/bigobj
+	/nologo	    # Don't show version info
+	/W3         # Enable warning level 3
+	/fp:fast    # Use fast floating point precision model
+	/Zc:wchar_t # Parse wchar_t as internal type
+	/GF         # Eliminate Duplicate Strings
+	/Gy         # Enable function level linking
+	/GR-        # Disable RTTI
+	/utf-8      # Set source and execution charset to utf-8
+	/Wv:18      # Disable warnings until SDK depedencies switch to UTF-8/ASCII.
+	/MP         # Build with multiple processes
+	/bigobj     # Allow larger .obj files
 
-	/WX  #Treat warnings as errors
+	/WX         # Treat warnings as errors
+	/wd4653     # Ignore PCH for any individual file that has different optimization settings
 )
 string(REPLACE ";" " " MSVC_COMMON_FLAGS "${MSVC_COMMON_FLAGS}")
  
@@ -30,13 +31,16 @@ set(CMAKE_CXX_FLAGS_DEBUG "/MDd /Zi /Zo /Od /Ob0 /Oy- /RTC1 /GS /DDEBUG /D_DEBUG
 set(CMAKE_C_FLAGS_PROFILE "/Zi /Zo /MD /Ox /Oy- /GS- /DNDEBUG /D_PROFILE" CACHE STRING "C Flags" FORCE)
 set(CMAKE_CXX_FLAGS_PROFILE "/Zi /Zo /MD /Ox /Oy- /GS- /DNDEBUG /D_PROFILE" CACHE STRING "C++ Flags" FORCE)
 
-set(CMAKE_C_FLAGS_RELEASE "/MD /Ox /GS- /DNDEBUG /D_RELEASE /DPURE_CLIENT" CACHE STRING "C Flags" FORCE)
-set(CMAKE_CXX_FLAGS_RELEASE "/MD /Ox /GS- /DNDEBUG /D_RELEASE /DPURE_CLIENT" CACHE STRING "C++ Flags" FORCE)
+set(CMAKE_C_FLAGS_RELEASE "/Zi /Zo /MD /Ox /GS- /DNDEBUG /D_RELEASE /DPURE_CLIENT" CACHE STRING "C Flags" FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "/Zi /Zo /MD /Ox /GS- /DNDEBUG /D_RELEASE /DPURE_CLIENT" CACHE STRING "C++ Flags" FORCE)
 
 set(CMAKE_SHARED_LINKER_FLAGS_PROFILE "/debug" CACHE STRING "Profile link flags" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_PROFILE    "/debug" CACHE STRING "Profile link flags" FORCE)
-
 set(CMAKE_MODULE_LINKER_FLAGS_PROFILE "/debug /INCREMENTAL" CACHE STRING "Profile link flags" FORCE)
+
+set(CMAKE_SHARED_LINKER_FLAGS_RELEASE "/debug" CACHE STRING "Release link flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE    "/debug" CACHE STRING "Release link flags" FORCE)
+set(CMAKE_MODULE_LINKER_FLAGS_RELEASE "/debug /INCREMENTAL" CACHE STRING "Release link flags" FORCE)
 
 function (wrap_whole_archive target source)
 	# no-op for MSVC (for now)
