df637d6a13f53195fa2b3f750e389f6a0934f312
Thomas Wollenzin <thomasw@crytek.de>
2017-05-09 11:35:51 +0200

    !XT (ACE) removed unused method (Approved by thomasw) Review: ID 65905 by Author: Thomas Wollenzin, Reviewer: Holger Durach, Observer: Samuel Kahn
    Copied from Perforce
     Change: 1513834

--- Code/Sandbox/Plugins/EditorAudioControlsEditor/InspectorPanel.h
+++ Code/Sandbox/Plugins/EditorAudioControlsEditor/InspectorPanel.h
@@ -12,20 +12,23 @@ namespace ACE
 class QConnectionsWidget;
 class CAudioAssetsManager;
 class IAudioSystemItem;
+class CAudioControl;
 
 class CInspectorPanel : public QFrame
 {
 	Q_OBJECT
 public:
+	
 	explicit CInspectorPanel(CAudioAssetsManager* pAssetsManager);
-	~CInspectorPanel();
+	virtual ~CInspectorPanel() override;
+
 	void Reload();
 
 public slots:
-	void SetSelectedControls(const std::vector<CAudioControl*>& selectedControls);
+	
+void SetSelectedControls(const std::vector<CAudioControl*>& selectedControls);
 
 private:
-	void UpdateConnectionData();
 
 	CAudioAssetsManager* m_pAssetsManager;
 	QConnectionsWidget*  m_pConnectionList;
