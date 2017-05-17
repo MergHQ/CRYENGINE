18ecf7365092ddaa549996d3e632e15e49cc78fc
Christian J�ger <christianj@crytek.com>
2017-05-17 10:40:18 +0200

    !XF (Sandbox) Add asset editor sample to sample plugin. (Approved by samuelk) !XB (Sandbox) Disable default compilation of SamplePlugin Review: ID 66099 by Author: Christian Jaeger, Reviewer: Vladimir Ulyanov, Observer: Samuel Kahn
    Copied from Perforce
     Change: 1516689

---
+++ Code/Sandbox/Plugins/SamplePlugin/SampleAssetType.cpp
@@ -0,0 +1,51 @@
+#include "StdAfx.h"
+#include "SampleAssetType.h"
+
+#include <AssetSystem/AssetEditor.h>
+#include <FilePathUtil.h>
+
+REGISTER_ASSET_TYPE(CSampleAssetType)
+
+namespace Private_SampleAssetType
+{
+
+// For the sake of simplicity, we directly use the C file API in this sample.
+// In a real-world scenario, however, you should prefer the ICryPak interface.
+
+bool CreateDataFile(const char* szFilePath)
+{
+	FILE* pFile;
+	if (fopen_s(&pFile, szFilePath, "w"))
+	{
+		return false;
+	}
+	fprintf(pFile, "hello, world");
+	fclose(pFile);
+	return true;
+}
+
+} // namespace Private_SampleAssetType
+
+bool CSampleAssetType::OnCreate(CEditableAsset& editAsset, const void* /* pCreateParams */) const
+{
+	using namespace Private_SampleAssetType;
+
+	const string basePath = PathUtil::RemoveExtension(PathUtil::RemoveExtension(editAsset.GetAsset().GetMetadataFile()));
+
+	const string dataFilePath = basePath + ".txt";
+	const string absoluteDataFilePath = PathUtil::Make(PathUtil::GetGameProjectAssetsPath(), dataFilePath);
+
+	if (!CreateDataFile(absoluteDataFilePath))
+	{
+		return false;
+	}
+
+	editAsset.AddFile(dataFilePath);
+
+	return true;
+}
+
+CAssetEditor* CSampleAssetType::Edit(CAsset* pAsset) const
+{
+	return CAssetEditor::OpenAssetForEdit("Sample Asset Editor", pAsset);
+}
