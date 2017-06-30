c83d3e83c4a95b56534197cd58bae8246ba324f1
Michael Bosschert <michaelbo@crytek.com>
2017-06-30 11:03:18 +0200

    !XB (CryVersionSelector) Fixed CPP templates looking for the executables in the wrong folder. !XB (CryVersionSelector) The --clean flag doesn't clear the __pycache__ so it has to be done manually.
    Copied from Perforce
     Change: 1529978

--- Tools/CryVersionSelector/setup.bat
+++ Tools/CryVersionSelector/setup.bat
@@ -1,3 +1,5 @@
+@ECHO OFF
+
 REM Tested with Python35
 REM Assumes Python 3.5 installed in c:\Python35
 
@@ -5,11 +7,33 @@ REM PyInstaller must be installed before
 REM To Install PyInstaller run:
 REM  c:\Python35\Scripts\pip.exe install PyInstaller
 
+REM Delete the __pycache__ folder first, since this isn't always updated properly.
+SET PYCACHE_DIR=%~dp0__pycache__\
+IF EXIST "%PYCACHE_DIR%" (
+	@ECHO Deleting __pycache__ folder...
+	@RD /S /Q "%PYCACHE_DIR%"
+	@ECHO Deleted cache.
+)
+
+REM Also delete the .spec files since they overwrite pyinstaller command arguments
+SET CRYRUN_SPEC=%~dp0cryrun.spec
+SET CRYSELECT_SPEC=%~dp0cryselect.spec
+IF EXIST "%CRYRUN_SPEC%" (
+	@ECHO Deleting cryrun.spec...
+	@DEL /F /Q "%CRYRUN_SPEC%"
+	@ECHO Deleted cryrun.spec.
+)
+
+IF EXIST "%CRYSELECT_SPEC%" (
+	@ECHO Deleting cryselect.spec...
+	@DEL /F /Q "%CRYSELECT_SPEC%"
+	@ECHO Deleted cryselect.spec.
+)
 
 REM Build CrySelect
-c:\Python35\Scripts\PyInstaller.exe cryselect.py --onefile --icon editor_icon16.ico --clean
+c:\Python35\Scripts\PyInstaller.exe --clean --onefile --icon editor_icon16.ico cryselect.py
 copy /Y dist\cryselect.exe cryselect.exe
 
 REM Build CryRun
-c:\Python35\Scripts\PyInstaller.exe cryrun.py --onefile --icon editor_icon16.ico --clean
+c:\Python35\Scripts\PyInstaller.exe --clean --onefile --icon editor_icon16.ico cryrun.py 
 copy /Y dist\cryrun.exe cryrun.exe
