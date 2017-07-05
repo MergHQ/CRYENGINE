975f55195f620af366033386dffd213411a1a176
Dominique Mader <dominique@crytek.de>
2017-07-05 22:43:12 +0200

    !XB (CE-12799)(CrySchematyc) Fixed order of object refresh and script compilation on element adds\removes. Review: ID 67106 by Author: Dominique Mader, Reviewer: Vladimir Ulyanov, Observer: Rok Erjavec, Observer: Luis Lairet  (Approved by thomasw)
    Copied from Perforce
     Change: 1531843

--- Code/Sandbox/Plugins/EditorSchematyc/ScriptBrowserWidget.cpp
+++ Code/Sandbox/Plugins/EditorSchematyc/ScriptBrowserWidget.cpp
@@ -839,7 +839,7 @@ void CScriptBrowserModel::OnScriptRegistryChange(const SScriptRegistryChange& ch
 		case EScriptRegistryChangeType::ElementAdded:
 			{
 				OnScriptElementAdded(change.element);
-				bRecompile = true;
+				bRecompile = false; // Happens in backend.
 				break;
 			}
 		case EScriptRegistryChangeType::ElementModified:
@@ -851,7 +851,7 @@ void CScriptBrowserModel::OnScriptRegistryChange(const SScriptRegistryChange& ch
 		case EScriptRegistryChangeType::ElementRemoved:
 			{
 				OnScriptElementRemoved(change.element);
-				bRecompile = true;
+				bRecompile = false; // Happens in backend.
 				break;
 			}
 		case EScriptRegistryChangeType::ElementSaved:
