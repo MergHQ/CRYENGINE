2395f68e4062a8cdbb2430c3594d8770f0c52b16
Pawel Wojtasik <pawelw@crytek.com>
2017-08-18 14:42:41 +0200

    !B (CE-13190) (GameTemplates) ThirdPersonShooter: Fixed warning spam issue occurring during camera movement by introducing more inertia when toggling the state of Mannequin tags. !B (CryDefaultEntities) Fixed unresponsive fragment transitions when calling CAdvancedAnimationComponent::QueueFragment(). Fixed a related memory leak issue. !B (CryDefaultEntities) Fixed CAdvancedAnimationComponent::SetMotionParameter() not respecing motion parameter overrides coming from higher-level systems. !T (GameTemplates) ThirdPersonShooter: Introduced a simple mouse input smoothing filter to mitigate jerky motion when turning around.
    Copied from Perforce
     Change: 1545482

--- Code/GameTemplates/cpp/ThirdPersonShooter/Code/Components/Player.h
+++ Code/GameTemplates/cpp/ThirdPersonShooter/Code/Components/Player.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <array>
+#include <numeric>
+
 #include <CryEntitySystem/IEntityComponent.h>
 #include <CryMath/Cry_Camera.h>
 
@@ -32,6 +35,57 @@ class CPlayerComponent final : public IEntityComponent
 		MoveBack = 1 << 3
 	};
 
+	template<typename T, size_t SAMPLES_COUNT>
+	class MovingAverage
+	{
+		static_assert(SAMPLES_COUNT > 0, "SAMPLES_COUNT shall be larger than zero!");
+
+	public:
+
+		MovingAverage()
+			: m_values()
+			, m_cursor(SAMPLES_COUNT)
+			, m_accumulator()
+		{
+		}
+
+		MovingAverage& Push(const T& value)
+		{
+			if (m_cursor == SAMPLES_COUNT)
+			{
+				m_values.fill(value);
+				m_cursor = 0;
+				m_accumulator = std::accumulate(m_values.begin(), m_values.end(), T(0));
+			}
+			else
+			{
+				m_accumulator -= m_values[m_cursor];
+				m_values[m_cursor] = value;
+				m_accumulator += m_values[m_cursor];
+				m_cursor = (m_cursor + 1) % SAMPLES_COUNT;
+			}
+
+			return *this;
+		}
+
+		T Get() const
+		{
+			return m_accumulator / T(SAMPLES_COUNT);
+		}
+
+		void Reset()
+		{
+			m_cursor = SAMPLES_COUNT;
+		}
+
+	private:
+
+		std::array<T, SAMPLES_COUNT> m_values;
+		size_t m_cursor;
+
+		T m_accumulator;
+	};
+
 public:
 	CPlayerComponent() = default;
 	virtual ~CPlayerComponent() {}
@@ -53,6 +107,7 @@ public:
 
 protected:
 	void UpdateMovementRequest(float frameTime);
+	void UpdateLookDirectionRequest(float frameTime);
 	void UpdateAnimation(float frameTime);
 	void UpdateCamera(float frameTime);
 
@@ -68,12 +123,17 @@ protected:
 	Cry::DefaultComponents::CAdvancedAnimationComponent* m_pAnimationComponent = nullptr;
 	Cry::DefaultComponents::CInputComponent* m_pInputComponent = nullptr;
 
-	TagID m_walkTagId;
+	FragmentID m_idleFragmentId;
+	FragmentID m_walkFragmentId;
+	TagID m_rotateTagId;
 
 	TInputFlags m_inputFlags;
-
 	Vec2 m_mouseDeltaRotation;
+	MovingAverage<Vec2, 10> m_mouseDeltaSmoothingFilter;
 
-	// Should translate to head orientation in the future
-	Quat m_lookOrientation;
+	FragmentID m_activeFragmentId;
+
+	Quat m_lookOrientation; //!< Should translate to head orientation in the future
+	float m_horizontalAngularVelocity;
+	MovingAverage<float, 10> m_averagedHorizontalAngularVelocity;
 };
