9d3e4d71e010e672b1608aaaad40c018fb6b5afc
Anton Knyazyev <anton@crytek.de>
2017-09-15 16:11:45 +0200

    !B (PhysDebugger) restored phys debugger compilation after cl 1553250, 1553481
    Copied from Perforce
     Change: 1554024

--- Code/Tools/PhysDebugger/CryMath/GeomQuery.h
+++ Code/Tools/PhysDebugger/CryMath/GeomQuery.h
@@ -9,6 +9,11 @@ struct CGeomExtent {
 	static int NumParts() { return 0; }
 	static int RandomPart(CRndGen&) { return 0; }
 	static float TotalExtent() { return 0; }
+	struct PartSum { 
+		Array<PosNorm> aPoints;
+		int iPart; 
+	};
+	static Array<PartSum> RandomPartsAliasSum(Array<PosNorm> aPoints, CRndGen& seed) { return Array<PartSum>(); }
 };
 struct CGeomExtents : CGeomExtent {
 	CGeomExtent& Make(EGeomForm) { return *this; }
@@ -21,8 +26,8 @@ inline float SphereExtent(EGeomForm,float) { return 0; }
 inline float CircleExtent(EGeomForm,float) { return 0; }
 inline float ScaleExtent(EGeomForm,float) { return 0; }
 inline Vec2 CircleRandomPoint(CRndGen&,EGeomForm,float) { return Vec2(0); }
-inline void BoxRandomPos(PosNorm&,CRndGen&,EGeomForm,const Vec3&) {}
-inline void SphereRandomPos(PosNorm&,CRndGen&,EGeomForm,float) {}
+inline void BoxRandomPoints(Array<PosNorm>,CRndGen&,EGeomForm,const Vec3&) {}
+inline void SphereRandomPoints(Array<PosNorm>,CRndGen&,EGeomForm,float) {}
 inline int TriMeshPartCount(EGeomForm,int) { return 0; }
 inline int TriIndices(int[3],int,EGeomForm) { return 0; }
 inline float TriExtent(EGeomForm, Vec3 const [3], Vec3 const&) { return 0; }
