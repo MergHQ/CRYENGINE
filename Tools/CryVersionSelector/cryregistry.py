5d62f0d2d190b8b8fc917f6f1d5bfaa2278de365
Michael Bosschert <michaelbo@crytek.com>
2017-09-15 11:20:58 +0200

    !XT (CryVersionSelector) Changed the loading order of cryengine.json files. !XR (CryVersionSelector) Cleaned up CryRegistry.py so it follows pep8 better.
    Copied from Perforce
     Change: 1553930

--- Tools/CryVersionSelector/cryregistry.py
+++ Tools/CryVersionSelector/cryregistry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''
+"""
 Handles loading and saving the available engines on this machine.
-'''
+"""
 
 import os
 import json
@@ -11,31 +11,32 @@ ENGINE_FILENAME = 'cryengine.json'
 ENGINE_EXTENSION = '.cryengine'
 
 def paths_engine_database():
-    '''
+    """
     Returns the path to the files where the engines are registered.
-    '''
+    """
     #https://blogs.msdn.microsoft.com/patricka/2010/03/18/where-should-i-store-my-data-and-configuration-files-if-i-target-multiple-os-versions/
-    if platform.system() == "Windows":
-        return [os.path.join(os.path.expandvars("%ALLUSERSPROFILE%"), "Crytek", "CRYENGINE", ENGINE_FILENAME)]
-    return [os.path.join(os.path.expandvars("$HOME"), ".cryengine", ENGINE_FILENAME)]
+    if platform.system() == 'Windows':
+        return [os.path.join(os.path.expandvars('%ALLUSERSPROFILE%'), 'Crytek', 'CRYENGINE', ENGINE_FILENAME)]
+    else:
+        return [os.path.join(os.path.expandvars('$HOME'), '.cryengine', ENGINE_FILENAME)]
 
 # For backwards compatibility the old paths are still looked up to load data, but saving is only done to paths_enginedb.
 def paths_engine_database_old():
-    '''
+    """
     Returns the path to the files where the engines used to be registered.
-    '''
+    """
 
-    if platform.system() == "Windows":
+    if platform.system() == 'Windows':
         return [
             os.path.join(os.path.expandvars('%USERPROFILE%'), '.cryengine', ENGINE_FILENAME),
-            os.path.join(os.path.expandvars("%LOCALAPPDATA%"), "Crytek", "CRYENGINE", ENGINE_FILENAME)
+            os.path.join(os.path.expandvars('%LOCALAPPDATA%'), 'Crytek', 'CRYENGINE', ENGINE_FILENAME)
         ]
     return []
 
 def delete():
-    '''
+    """
     Deletes all databases with registered engines.
-    '''
+    """
     for registry_path in paths_engine_database_old():
         try:
             if os.path.isfile(registry_path):
@@ -51,34 +52,38 @@ def delete():
             pass
 
 def load_engines():
-    '''
-    Returns a database of all registered engines.
-    '''
+    """
+    Returns a database of all registered engines. It loads the newest
+    locations first, and returns as soon as a valid file is found.
+    """
     database = {}
 
-    #Load old engine data first.
-    for registry_path in paths_engine_database_old():
-        try:
-            if os.path.isfile(registry_path):
-                with open(registry_path, 'r') as file:
-                    database.update(json.loads(file.read()))
-        except:
-            pass
-
+    # First try to load the newest files.
     for registry_path in paths_engine_database():
         try:
             if os.path.isfile(registry_path):
                 with open(registry_path, 'r') as file:
                     database.update(json.loads(file.read()))
+                    return database
+        except:
+            pass
+
+    # Load old engine data only if the newest data doesn't exist yet.
+    for registry_path in paths_engine_database_old():
+        try:
+            if os.path.isfile(registry_path):
+                with open(registry_path, 'r') as file:
+                    database.update(json.loads(file.read()))
+                    return database
         except:
             pass
 
     return database
 
 def save_engines(database):
-    '''
+    """
     Saves the database to the hard-drive.
-    '''
+    """
     for registry_path in paths_engine_database():
         try:
             registry_dir = os.path.dirname(registry_path)
@@ -88,13 +93,13 @@ def save_engines(database):
             with open(registry_path, 'w') as file:
                 file.write(json.dumps(database, indent=4, sort_keys=True))
         except:
-            print("Error while saving  to " + registry_path)
+            print('Error while saving  to {}.'.format(registry_path))
 
 def engine_path(database, engine_version):
-    '''
+    """
     Returns the path to the root-folder of the specified engine version,
     or None if the engine version is not in the database.
-    '''
+    """
     path = database.get(engine_version, {'uri': None})['uri']
 
     if not path:
