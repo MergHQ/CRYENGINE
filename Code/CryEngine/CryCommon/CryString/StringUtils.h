3e7e618885d5dd01e9f047d86b9892edbc4e4c4e
Anton Knyazyev <anton@crytek.de>
2017-09-27 19:47:21 +0200

    !O (Physics,Animation) allow more leeway in character physics primitive approximation; restored primitive keywords in bone poperties; tweaked capsule detection and primitive order
    Copied from Perforce
     Change: 1558592

--- Code/CryEngine/CryCommon/CryString/StringUtils.h
+++ Code/CryEngine/CryCommon/CryString/StringUtils.h
@@ -142,7 +142,7 @@ inline const char* strnstr(const char* szString, const char* szSubstring, int nS
 	if (!nSubstringLength)
 		return szString;
 
-	for (int nSubstringPos = 0; szString[nSubstringPos] && nSubstringPos < nSuperstringLength - nSubstringLength; ++nSubstringPos)
+	for (int nSubstringPos = 0; szString[nSubstringPos] && nSubstringPos <= nSuperstringLength - nSubstringLength; ++nSubstringPos)
 	{
 		if (strncmp(szString + nSubstringPos, szSubstring, nSubstringLength) == 0)
 			return szString + nSubstringPos;
