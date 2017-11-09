669d39b67c8f5f492daaf6d92dbba28fe602e863
Niels Fröhling <nielsf@crytek.de>
2017-11-09 11:54:52 +0100

    !B (Renderer) Fixed DX12 async upload violation (initial data for CGpuBuffer)
    Copied from Perforce
     Change: 1595581

--- Code/CryEngine/RenderDll/XRenderD3D9/DX12/Resource/Texture/CCryDX12Texture2D.cpp
+++ Code/CryEngine/RenderDll/XRenderD3D9/DX12/Resource/Texture/CCryDX12Texture2D.cpp
@@ -191,6 +191,11 @@ CCryDX12Texture2D* CCryDX12Texture2D::Create(CCryDX12Device* pDevice, const FLOA
 	}
 
 	ID3D12Resource* resource = NULL;
+	if (pInitialData)
+	{
+		// Anticipate deferred initial upload
+		resourceUsage = D3D12_RESOURCE_STATE_COPY_DEST;
+	}
 
 	HRESULT hresult = S_OK;
 	if (pDesc->MiscFlags & D3D11_RESOURCE_MISC_HIFREQ_HEAP)
