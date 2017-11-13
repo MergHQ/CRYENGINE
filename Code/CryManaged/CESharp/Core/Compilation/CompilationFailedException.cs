ca32ea67a87094313024303952535a2bc0f08020
Michael Bosschert <michaelbo@crytek.com>
2017-11-13 15:43:16 +0100

    !XR (C# Tests) Changed type of exception to better reflect what is going wrong.
    Copied from Perforce
     Change: 1596926

--- Code/CryManaged/CESharp/Core/Compilation/CompilationFailedException.cs
+++ Code/CryManaged/CESharp/Core/Compilation/CompilationFailedException.cs
@@ -4,13 +4,28 @@ using System;
 
 namespace CryEngine.Compilation
 {
-	internal class CompilationFailedException : Exception
+	/// <summary>
+	/// Exception indicating that compiling C# source files has failed.
+	/// </summary>
+	public class CompilationFailedException : Exception
 	{
+		/// <summary>
+		/// Exception indicating that compiling C# source files has failed.
+		/// </summary>
 		public CompilationFailedException() { }
 
+		/// <summary>
+		/// Exception indicating that compiling C# source files has failed.
+		/// </summary>
+		/// <param name="errorMessage"></param>
 		public CompilationFailedException(string errorMessage)
 			: base(errorMessage) { }
 
+		/// <summary>
+		/// Exception indicating that compiling C# source files has failed.
+		/// </summary>
+		/// <param name="errorMessage"></param>
+		/// <param name="innerEx"></param>
 		public CompilationFailedException(string errorMessage, Exception innerEx)
 			: base(errorMessage, innerEx) { }
 	}
