7d0b783cab471893d9290941e9c6a21d1ab6ad56
Scott Peter <scott@crytek.de>
2017-11-24 13:58:19 +0100

    !B (Wavicle) Fixed non-spawning of certain effects, that incorrectly had restart periods. They now start just once, as before.
    Copied from Perforce
     Change: 1601484

--- Code/CryEngine/Cry3DEngine/ParticleSystem/Features/FeatureSpawn.cpp
+++ Code/CryEngine/Cry3DEngine/ParticleSystem/Features/FeatureSpawn.cpp
@@ -119,11 +119,17 @@ protected:
 		const CParticleEmitter* pEmitter = context.m_runtime.GetEmitter();
 		if (pEmitter->IsIndependent())
 		{
-			if (!runtime.IsChild() && context.m_params.IsImmortal())
-				return;
+			if (!runtime.IsChild())
+			{
+				// Skip spawning immortal independent top-level effects
+				float maxLifetime = m_delay.GetValueRange().end + m_duration.GetValueRange().end + context.m_params.m_maxParticleLifeTime;
+				if (!std::isfinite(maxLifetime))
+					return;
+			}
 		}
 		else if (m_restart.IsEnabled())
 		{
+			// Perform restarts only on 
 			THeapArray<uint> indicesArray(*context.m_pMemHeap);
 			indicesArray.reserve(numInstances);
 
