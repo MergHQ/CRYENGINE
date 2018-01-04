b63f16c0a85e519f747fd31d11b6f03a87e55a1b
Alexander Klinger <alexanderkl@crytek.de>
2018-01-04 17:09:17 +0100

    !B (CE-14944) (DefaultEntities) Projector Light is floating above the ground when dragged into the level
    Copied from Perforce
     Change: 1632540

--- Code/CryPlugins/CryDefaultEntities/Module/DefaultComponents/Lights/ProjectorLightComponent.h
+++ Code/CryPlugins/CryDefaultEntities/Module/DefaultComponents/Lights/ProjectorLightComponent.h
@@ -52,7 +52,7 @@ namespace Cry
 				desc.SetLabel("Projector Light");
 				desc.SetDescription("Emits light from its position in a general direction, constrained to a specified angle");
 				desc.SetIcon("icons:ObjectTypes/light.ico");
-				desc.SetComponentFlags({ IEntityComponent::EFlags::Transform, IEntityComponent::EFlags::Socket, IEntityComponent::EFlags::Attach, IEntityComponent::EFlags::ClientOnly });
+				desc.SetComponentFlags({ IEntityComponent::EFlags::Transform, IEntityComponent::EFlags::Socket, IEntityComponent::EFlags::Attach, IEntityComponent::EFlags::ClientOnly, IEntityComponent::EFlags::NoCreationOffset });
 
 				desc.AddMember(&CProjectorLightComponent::m_bActive, 'actv', "Active", "Active", "Determines whether the light is enabled", true);
 				desc.AddMember(&CProjectorLightComponent::m_radius, 'radi', "Radius", "Range", "Determines whether the range of the point light", 10.f);
