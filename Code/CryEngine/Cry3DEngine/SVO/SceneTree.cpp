646e5ee5fe21a9ae8be61bf274ac8644e1682ce6
Vladimir Kajalin <vlad@crytek.de>
2018-01-19 17:54:38 +0100

    !B (CE-14986) Fixed GI system freeze during result screen !R (Sandbox) Added ShadowsSoftness parameter into GI settings UI !B (Shaders) Fixed too dark SS shadows from the grass
    Copied from Perforce
     Change: 1640242

--- Code/CryEngine/Cry3DEngine/SVO/SceneTree.cpp
+++ Code/CryEngine/Cry3DEngine/SVO/SceneTree.cpp
@@ -2270,6 +2270,7 @@ void C3DEngine::LoadTISettings(XmlNodeRef pInputNode)
 	GetCVars()->e_svoTI_SSDepthTrace = (float)atof(GetXMLAttribText(pInputNode, szXmlNodeName, "SSDepthTrace", "0"));
 
 	GetCVars()->e_svoTI_ShadowsFromSun = (int)atof(GetXMLAttribText(pInputNode, szXmlNodeName, "ShadowsFromSun", "0"));
+	GetCVars()->e_svoTI_ShadowsSoftness = (float)atof(GetXMLAttribText(pInputNode, szXmlNodeName, "ShadowsSoftness", "0"));
 	GetCVars()->e_svoTI_Troposphere_Active = (int)atof(GetXMLAttribText(pInputNode, szXmlNodeName, "Troposphere_Active", "0"));
 	GetCVars()->e_svoTI_Troposphere_Brightness = (float)atof(GetXMLAttribText(pInputNode, szXmlNodeName, "Troposphere_Brightness", "0"));
 	GetCVars()->e_svoTI_Troposphere_Ground_Height = (float)atof(GetXMLAttribText(pInputNode, szXmlNodeName, "Troposphere_Ground_Height", "0"));
