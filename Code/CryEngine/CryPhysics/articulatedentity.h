f27a1196c3827c4cf9df30c64497812e2b211213
Anton Knyazyev <anton@crytek.de>
2018-02-13 14:55:24 +0100

    !B (Physics) fixed articulated entity constraints broken by cl 1651254
    Copied from Perforce
     Change: 1653087

--- Code/CryEngine/CryPhysics/articulatedentity.h
+++ Code/CryEngine/CryPhysics/articulatedentity.h
@@ -139,7 +139,9 @@ class CArticulatedEntity : public CRigidEntity {
 	virtual float GetDamping(float time_interval);
 	virtual void GetSleepSpeedChange(int ipart, Vec3 &v,Vec3 &w) { int i=m_infos[ipart].iJoint; v=m_joints[i].vSleep; w=m_joints[i].wSleep; }
 	virtual void OnHostSync(CPhysicalEntity *pHost);
-	virtual int HasConstraintContactsWith(const CPhysicalEntity *pent, int flagsIgnore=0) const { return pent==m_pHost && !(flagsIgnore & constraint_inactive); }
+	virtual int HasConstraintContactsWith(const CPhysicalEntity *pent, int flagsIgnore=0) const { 
+		return pent==m_pHost && !(flagsIgnore & constraint_inactive) ? true : CRigidEntity::HasConstraintContactsWith(pent,flagsIgnore); 
+	}
 
 	virtual int GetPotentialColliders(CPhysicalEntity **&pentlist, float dt=0);
 	virtual int CheckSelfCollision(int ipart0,int ipart1);
