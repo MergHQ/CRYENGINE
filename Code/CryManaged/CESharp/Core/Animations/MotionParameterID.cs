be474184b03901c679abc46202a9eba888a60d08
Build <buildengineers@crytek.de>
2018-03-19 19:18:45 +0100

    !XT (Code) Update copyright headers in Code/CryManaged, Code/CryPlugins, and Code/CryExtensions.
    Copied from Perforce
     Change: 1670414

--- Code/CryManaged/CESharp/Core/Animations/MotionParameterID.cs
+++ Code/CryManaged/CESharp/Core/Animations/MotionParameterID.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2001-2016 Crytek GmbH / Crytek Group. All rights reserved.
+// Copyright 2001-2018 Crytek GmbH / Crytek Group. All rights reserved.
 
 namespace CryEngine.Animations
 {
--- Code/CryPlugins/CryLobby/Module/CryReward.h
+++ Code/CryPlugins/CryLobby/Module/CryReward.h
@@ -1,4 +1,4 @@
-// Copyright 2001-2016 Crytek GmbH / Crytek Group. All rights reserved.
+// Copyright 2001-2018 Crytek GmbH / Crytek Group. All rights reserved.
 
 #if !defined(__CRYREWARD_H__)
 #define __CRYREWARD_H__
