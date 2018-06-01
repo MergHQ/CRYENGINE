0181c8e6fb25ed21d36686b3a579608456840454
Michael Bosschert <michaelbo@crytek.com>
2018-06-01 10:30:02 +0200

    !B (CE-16910) (C#) C# PhysicsEntity does not return the right value for PhysicalizationType after it has been physicalized.
    Copied from Perforce
     Change: 1701898

--- Code/CryManaged/CESharp/Core/EntitySystem/PhysicsEntity.cs
+++ Code/CryManaged/CESharp/Core/EntitySystem/PhysicsEntity.cs
@@ -74,7 +74,7 @@ namespace CryEngine.EntitySystem
 		/// </summary>
 		/// <value>The physicalized type of the <see cref="PhysicsObject"/>.</value>
 		[SerializeValue]
-		public PhysicalizationType PhysicsType{ get; set;} = PhysicalizationType.None;
+		public PhysicalizationType PhysicsType{ get; protected set;} = PhysicalizationType.None;
 
 		/// <summary>
 		/// Gets the Entity that this <see cref="PhysicsObject"/> belongs to. 
@@ -360,10 +360,12 @@ namespace CryEngine.EntitySystem
 				return;
 			}
 
-			var physParams = new SEntityPhysicalizeParams();
-			physParams.mass = mass;
-			physParams.type = (int)type;
-			OwnerEntity.NativeHandle.Physicalize(physParams);
+			OwnerEntity.NativeHandle.Physicalize(new SEntityPhysicalizeParams
+			{
+				mass = mass,
+				type = (int)type
+			});
+			PhysicsType = type;
 		}
 
 		/// <summary>
@@ -387,12 +389,12 @@ namespace CryEngine.EntitySystem
 		[Obsolete("Using EPhysicalizizationType is obsolete. Use PhysicalizationType instead.")]
 		public void Physicalize(float mass, int density, EPhysicalizationType type)
 		{
-			var physParams = new SEntityPhysicalizeParams();
-			physParams.mass = mass;
-			physParams.density = density;
-			physParams.type = (int)type;
-
-			OwnerEntity.NativeHandle.Physicalize(physParams);
+			OwnerEntity.NativeHandle.Physicalize(new SEntityPhysicalizeParams
+			{
+				mass = mass,
+				density = density,
+				type = (int)type
+			});
 			SetType((pe_type)type);
 		}
 
