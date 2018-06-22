6017a8563c62f582577d86cb08e89a0eb4949a2c
Michael Madsen <michaelma@crytek.de>
2018-06-22 14:27:04 +0200

    !XR (CMake) Use common batch file for all platforms; change platform-specific batch files to refer to this instead !XT (CMake) Allow VS version choice for all platforms !XT (CMake) Define batch file platforms in stand-alone files
    Copied from Perforce
     Change: 1714849

--- Tools/CMake/cmake_create_nsight_android_solution.bat
+++ Tools/CMake/cmake_create_nsight_android_solution.bat
@@ -1,12 +1,6 @@
 @echo off
-pushd "%~dp0\..\.."
-mkdir solutions_cmake\android > nul 2>&1
-cd solutions_cmake\android
-..\..\Tools\CMake\Win32\bin\cmake -G "Visual Studio 14 2015" -D CMAKE_TOOLCHAIN_FILE=Tools\CMake\toolchain\android\Android-Nsight.cmake ..\..
-if ERRORLEVEL 1 (
-	pause
-) else (
-	echo Starting cmake-gui...
-	start ..\..\Tools\CMake\Win32\bin\cmake-gui .
-)
+setlocal
+pushd %~dp0
+for /f "tokens=1,2 delims==" %%j in (toolchain\android.platform) do set %%j=%%k
+call cmake_create_solution.bat
 popd
