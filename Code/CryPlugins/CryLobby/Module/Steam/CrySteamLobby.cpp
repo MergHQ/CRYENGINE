23e8ec24b51718f4bbf5715aca9883a752521053
Luca Vezzaro <luca@crytek.com>
2018-07-11 12:44:56 +0200

    !B (CE-15543) (SteamLobby) Starting the CE Gamelauncher triggers an assert
    Copied from Perforce
     Change: 1726206

--- Code/CryPlugins/CryLobby/Module/Steam/CrySteamLobby.cpp
+++ Code/CryPlugins/CryLobby/Module/Steam/CrySteamLobby.cpp
@@ -183,10 +183,13 @@ ECryLobbyError CCrySteamLobbyService::Initialise(ECryLobbyServiceFeatures featur
 	if (SteamUser() == NULL)
 	{
 		Cry::GamePlatform::IPlugin* pPlugin = gEnv->pSystem->GetIPluginManager()->QueryPlugin<Cry::GamePlatform::IPlugin>();
-		CRY_ASSERT(
-			(pPlugin != nullptr) &&
-			(pPlugin->GetMainService() != nullptr) &&
-			(pPlugin->GetMainService()->GetServiceIdentifier() == Cry::GamePlatform::SteamServiceID));
+		
+		if ((pPlugin == nullptr) ||
+			(pPlugin->GetMainService() == nullptr) || 
+			(pPlugin->GetMainService()->GetServiceIdentifier() != Cry::GamePlatform::SteamServiceID))
+		{
+			ret = eCLE_NotInitialised;
+		}
 
 		if (ret != eCLE_Success)
 		{
