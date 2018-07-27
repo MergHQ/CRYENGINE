dfae8968e63a1e35a834fd0ac1a1627b89157e6d
Niels Fröhling <nielsf@crytek.de>
2018-07-27 15:12:33 +0200

    !B (CE-17176) (Renderer) Fixed missing information in water volume reflections
    Copied from Perforce
     Change: 1826878

--- Code/CryEngine/RenderDll/XRenderD3D9/GraphicsPipeline/Water.h
+++ Code/CryEngine/RenderDll/XRenderD3D9/GraphicsPipeline/Water.h
@@ -74,13 +74,13 @@ public:
 
 	enum EPerPassSampler
 	{
-		ePerPassSampler_Aniso16xWrap = 1,
+		ePerPassSampler_Aniso16xWrap    = 0,
+		ePerPassSampler_Aniso16xClamp   = 1,
 
-		ePerPassSampler_PointWrap    = EFSS_MAX,
-		ePerPassSampler_PointClamp,
+		ePerPassSampler_PointWrap       = 8,
+		ePerPassSampler_PointClamp      = 9,
 
-		ePerPassSampler_Aniso16xClamp,
-		ePerPassSampler_LinearClampComp,
+		ePerPassSampler_LinearClampComp = 10,
 
 		ePerPassSampler_Count,
 	};
@@ -180,6 +180,10 @@ private:
 
 	uint64 m_frameIdWaterSim;
 	Vec4   m_oceanAnimationParams[2];
+
+	int32 m_aniso16xClampSampler;
+	int32 m_aniso16xWrapSampler;
+	int32 m_linearCompareClampSampler;
 
 	bool              m_bWaterNormalGen;
 	bool              m_bOceanMaskGen;
