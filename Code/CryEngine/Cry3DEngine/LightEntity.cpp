839c7261652a4173c1a8fbb8b09845272ab5b385
Shlomi Steinberg-Vaisburd <shlomi@crytek.de>
2018-08-28 16:54:27 +0200

    !B (CE-17787) (Renderer) Fix projector lights (integration issue)
    Copied from Perforce
     Change: 1842363

--- Code/CryEngine/Cry3DEngine/LightEntity.cpp
+++ Code/CryEngine/Cry3DEngine/LightEntity.cpp
@@ -1375,6 +1375,7 @@ void CLightEntity::InitShadowFrustum_PROJECTOR(ShadowMapFrustum* pFr, int dwAllo
 
 	pFr->fNearDist = 0.01f;
 	pFr->fFarDist = m_light.m_fRadius;
+	pFr->nOmniFrustumMask.set(0);
 	pFr->bUseShadowsPool = (m_light.m_Flags & DLF_DEFERRED_LIGHT) != 0;
 
 	// set texture size
