398189c6666480ee332c150aded575fae2339e00
Morteza Mostajab <seyedmorteza@crytek.com>
2018-09-03 15:20:14 +0200

    !B (CMake) Fixes linking error to DXC.
    Copied from Perforce
     Change: 1844523

--- Tools/CMake/modules/DXC.cmake
+++ Tools/CMake/modules/DXC.cmake
@@ -11,7 +11,7 @@ if(WIN64)
 
     set_target_properties(DXC PROPERTIES IMPORTED_LOCATION "${SDK_DIR}/DXC/bin/dxcompiler.dll")
     set_target_properties(DXC PROPERTIES IMPORTED_IMPLIB "${SDK_DIR}/DXC/lib/dxcompiler.lib")
-    set_target_properties(DXC PROPERTIES INTERFACE_LINK_LIBRARIES "${SDK_DIR}DXC/lib/dxcompiler.lib")
+    set_target_properties(DXC PROPERTIES INTERFACE_LINK_LIBRARIES "${SDK_DIR}/DXC/lib/dxcompiler.lib")
     deploy_runtime_files("${SDK_DIR}/DXC/bin/dxcompiler.dll")
 elseif(WIN32)
     message( FATAL_ERROR "DXC is not compiled for win32 at the moment." )
