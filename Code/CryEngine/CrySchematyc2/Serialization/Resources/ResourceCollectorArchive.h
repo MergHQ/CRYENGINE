a669b67953a73a1057ea900cad351e9b0d83cfca
Pavlo Ilin <pavloi@crytek.de>
2018-09-05 10:20:24 +0200

    !B (Schematyc) Fix asserts due to the missing method overrides in CResourceCollectorArchive
    Copied from Perforce
     Change: 1845756

--- Code/CryEngine/CrySchematyc2/Serialization/Resources/ResourceCollectorArchive.h
+++ Code/CryEngine/CrySchematyc2/Serialization/Resources/ResourceCollectorArchive.h
@@ -23,13 +23,17 @@ namespace Schematyc2
 
 		// Serialization::IArchive
 		virtual bool operator () (bool& value, const char* szName = "", const char* szLabel = nullptr) override;
+		virtual bool operator () (char& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (int8& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (uint8& value, const char* szName = "", const char* szLabel = nullptr) override;
+		virtual bool operator () (int16& value, const char* szName = "", const char* szLabel = nullptr) override;
+		virtual bool operator () (uint16& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (int32& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (uint32& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (int64& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (uint64& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (float& value, const char* szName = "", const char* szLabel = nullptr) override;
+		virtual bool operator () (double& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (Serialization::IString& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (const Serialization::SStruct& value, const char* szName = "", const char* szLabel = nullptr) override;
 		virtual bool operator () (Serialization::IContainer& value, const char* szName = "", const char* szLabel = nullptr) override;
