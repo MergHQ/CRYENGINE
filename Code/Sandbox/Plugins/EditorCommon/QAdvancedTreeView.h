c378bb9f3cb83049ee60544ab6c96cde8bdd0e9e
Luis Manuel Lairet Montero <luis@crytek.de>
2018-09-10 10:35:05 +0200

    !F (Sandbox) (DEV-6236) QAdvancedTreeView expands items when hovered with drag and drop event
    Copied from Perforce
     Change: 1848108

--- Code/Sandbox/Plugins/EditorCommon/QAdvancedTreeView.h
+++ Code/Sandbox/Plugins/EditorCommon/QAdvancedTreeView.h
@@ -12,6 +12,7 @@
 #include <CrySandbox/CrySignal.h>
 
 class QAdvancedItemDelegate;
+class QTimer;
 
 //These will persist through reset as long as your internalId/internalPointers are stable through resets
 class EDITOR_COMMON_API CAdvancedPersistentModelIndex
@@ -52,62 +53,67 @@ public:
 	virtual void setModel(QAbstractItemModel* model) override;
 	virtual void reset() override;
 
-	void                ConnectDefaultHeaderContextMenu();
-	void                DisconnectDefaultHeaderContextMenu();
+	void         ConnectDefaultHeaderContextMenu();
+	void         DisconnectDefaultHeaderContextMenu();
 
 	//! Set once, this is not expected to change
-	void                SetAttributeRole(int role = Attributes::s_getAttributeRole, int menuPathRole = Attributes::s_attributeMenuPathRole);
+	void                   SetAttributeRole(int role = Attributes::s_getAttributeRole, int menuPathRole = Attributes::s_attributeMenuPathRole);
 
-	int                 GetColumnCount() const;
-	bool                IsColumnVisible(int i) const;
-	void                SetColumnVisible(int i, bool visible = true);
-	
-	virtual QVariantMap GetState() const override;
-	virtual void        SetState(const QVariantMap& state) override;
+	int                    GetColumnCount() const;
+	bool                   IsColumnVisible(int i) const;
+	void                   SetColumnVisible(int i, bool visible = true);
+
+	virtual QVariantMap    GetState() const override;
+	virtual void           SetState(const QVariantMap& state) override;
 
 	QAdvancedItemDelegate* GetAdvancedDelegate();
 
-	void                BackupExpanded();
-	void                RestoreExpanded();
-	void				BackupSelection();
-	void				RestoreSelection();
+	void                   BackupExpanded();
+	void                   RestoreExpanded();
+	void                   BackupSelection();
+	void                   RestoreSelection();
 
-	void                TriggerRefreshHeaderColumns();
+	void                   TriggerRefreshHeaderColumns();
 
 	//Utilities that may be useful outside of QAdvancedTreeView
 	static QModelIndex ToOriginal(const QModelIndex& index, QAbstractItemModel* viewModel);
 	static QModelIndex FromOriginal(const QModelIndex& index, QAbstractItemModel* viewModel);
-	static void BackupSelection(std::vector<CAdvancedPersistentModelIndex>& selectedBackupStorage, QAbstractItemModel* viewModel, QItemSelectionModel* viewSelectionModel);
-	static void RestoreSelection(std::vector<CAdvancedPersistentModelIndex>& selectedBackupStorage, QAbstractItemModel* viewModel, QItemSelectionModel* viewSelectionModel);
+	static void        BackupSelection(std::vector<CAdvancedPersistentModelIndex>& selectedBackupStorage, QAbstractItemModel* viewModel, QItemSelectionModel* viewSelectionModel);
+	static void        RestoreSelection(std::vector<CAdvancedPersistentModelIndex>& selectedBackupStorage, QAbstractItemModel* viewModel, QItemSelectionModel* viewSelectionModel);
 
 	CCrySignal<void()> signalVisibleSectionCountChanged;
 
 protected:
 	virtual void startDrag(Qt::DropActions supportedActions) override;
-	virtual void drawBranches(QPainter *painter,
-		const QRect &rect,
-		const QModelIndex &index) const override;
-	virtual void drawBranchIndicator(QPainter *painter, const QStyleOptionViewItem *opt) const;
+	virtual void drawBranches(QPainter* painter, const QRect& rect, const QModelIndex& index) const override;
+	virtual void drawBranchIndicator(QPainter* painter, const QStyleOptionViewItem* opt) const;
+
 	virtual void mousePressEvent(QMouseEvent* pEvent) override;
-	bool viewportEvent(QEvent *event);
+	virtual bool viewportEvent(QEvent* event) override;
+
+	virtual void dragEnterEvent(QDragEnterEvent* pEvent) override;
+	virtual void dragLeaveEvent(QDragLeaveEvent* pEvent) override;
+	virtual void dragMoveEvent(QDragMoveEvent* pEvent) override;
+	virtual void dropEvent(QDropEvent* pEvent) override;
 
 	static constexpr int s_iconColumnWidth = 25;
 	static constexpr int m_BranchIconSize = 16;
 
 private:
 
-	void        OnExpanded(const QModelIndex& index);
-	void        OnCollapsed(const QModelIndex& index);
-	void        OnContextMenu(const QPoint& point);
+	void OnExpanded(const QModelIndex& index);
+	void OnCollapsed(const QModelIndex& index);
+	void OnContextMenu(const QPoint& point);
+	void ClearDragHandlerData();
 
-	void        OnModelAboutToBeReset();
-	void        OnModelReset();
-	void        OnColumnsInserted(const QModelIndex& parent, int first, int last);
-	void        OnColumnsRemoved(const QModelIndex& parent, int first, int last);
-	void        OnColumnsMoved(const QModelIndex& parent, int start, int end, const QModelIndex& destination, int column);
-	void        OnHeaderSectionCountChanged(int oldCount, int newCount);
+	void OnModelAboutToBeReset();
+	void OnModelReset();
+	void OnColumnsInserted(const QModelIndex& parent, int first, int last);
+	void OnColumnsRemoved(const QModelIndex& parent, int first, int last);
+	void OnColumnsMoved(const QModelIndex& parent, int start, int end, const QModelIndex& destination, int column);
+	void OnHeaderSectionCountChanged(int oldCount, int newCount);
 
-	void		UpdateColumnVisibility(bool reset = false);
+	void UpdateColumnVisibility(bool reset = false);
 
 	BehaviorFlags                              m_behavior;
 	QSet<QPersistentModelIndex>                m_expanded;
@@ -122,5 +128,7 @@ private:
 
 	// column state merging map, to keep states for columns not present in all models
 	QVariantMap m_columnStateMap;
-	const int m_layoutVersion;
+	const int   m_layoutVersion;
+	QModelIndex m_hoveredIndex;
+	QTimer*     m_pExpandOnDragTimer;
 };
