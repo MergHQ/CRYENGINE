beceaba50d3052cdd056a8c7d9bc626f0fc9a2a9
Marius Popczyk <marius@crytek.de>
2018-09-18 14:18:39 +0200

    !XT (Audio) Added debug option to draw the activity radius. !XT (Audio) Added checkbox to enable/disable the stoptrigger. !XT (Audio) Removed GetActivityRadius function !XB (Audio) StopTrigger was executed when properties have chnaged.
    Copied from Perforce
     Change: 1852611

--- Code/CryPlugins/CryDefaultEntities/Module/DefaultComponents/Audio/TriggerComponent.cpp
+++ Code/CryPlugins/CryDefaultEntities/Module/DefaultComponents/Audio/TriggerComponent.cpp
@@ -4,12 +4,57 @@
 #include "TriggerComponent.h"
 #include <CryAudio/IAudioSystem.h>
 
+#if defined(INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE)
+	#include <CryRenderer/IRenderAuxGeom.h>
+	#include <CrySerialization/Decorators/ActionButton.h>
+#endif  // INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE
+
 namespace Cry
 {
 namespace Audio
 {
 namespace DefaultComponents
 {
+//////////////////////////////////////////////////////////////////////////
+inline void ReflectType(Schematyc::CTypeDesc<SPlayTriggerSerializeHelper>& desc)
+{
+	desc.SetGUID("ABB005B4-36C5-4120-ABFA-294F22E496C8"_cry_guid);
+}
+
+//////////////////////////////////////////////////////////////////////////
+inline void SPlayTriggerSerializeHelper::Serialize(Serialization::IArchive& archive)
+{
+	archive(m_autoPlay, "autoPlay", "AutoPlay");
+	archive(Serialization::AudioTrigger<string>(m_name), "playTrigger", "PlayTrigger");
+
+	if (archive.isInput())
+	{
+		m_id = CryAudio::StringToId(m_name.c_str());
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+inline void ReflectType(Schematyc::CTypeDesc<SStopTriggerSerializeHelper>& desc)
+{
+	desc.SetGUID("0BF70B52-8DA8-41AA-9F01-1F556291871E"_cry_guid);
+}
+
+//////////////////////////////////////////////////////////////////////////
+inline void SStopTriggerSerializeHelper::Serialize(Serialization::IArchive& archive)
+{
+	archive(m_canStop, "canStop", "Can Stop");
+
+	if (m_canStop)
+	{
+		archive(Serialization::AudioTrigger<string>(m_name), "stopTrigger", "StopTrigger");
+
+		if (archive.isInput())
+		{
+			m_id = CryAudio::StringToId(m_name.c_str());
+		}
+	}
+}
+
 //////////////////////////////////////////////////////////////////////////
 static void ReflectType(Schematyc::CTypeDesc<CTriggerComponent::SFinishedSignal>& desc)
 {
@@ -26,17 +71,11 @@ void CTriggerComponent::Register(Schematyc::CEnvRegistrationScope& componentScop
 		pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
 		componentScope.Register(pFunction);
 	}
+
 	{
 		auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CTriggerComponent::Stop, "11AB550A-A6C9-422E-8D87-8333E845C23D"_cry_guid, "Stop");
 		pFunction->SetDescription("Stops the PlayTrigger if no StopTrigger assigned otherwise executes the StopTrigger.");
 		pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
-		componentScope.Register(pFunction);
-	}
-	{
-		auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CTriggerComponent::GetActivityRadius, "F70A0C3D-50EC-4858-A470-2855FFEC25E2"_cry_guid, "GetActivityRadius");
-		pFunction->SetDescription("Retrieves the activity radius of this component.");
-		pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
-		pFunction->BindOutput(1, 'radi', "Radius");
 		componentScope.Register(pFunction);
 	}
 
@@ -53,14 +92,11 @@ void CTriggerComponent::ReflectType(Schematyc::CTypeDesc<CTriggerComponent>& des
 	desc.SetIcon("icons:Audio/component_trigger.ico");
 	desc.SetComponentFlags({ IEntityComponent::EFlags::Transform, IEntityComponent::EFlags::Attach, IEntityComponent::EFlags::ClientOnly });
 
-	desc.AddMember(&CTriggerComponent::m_autoPlay, 'auto', "autoPlay", "AutoPlay", "Automatically executes PlayTrigger when enabled.", true);
-
-	desc.AddMember(&CTriggerComponent::m_playTrigger, 'tri1', "playTrigger", "PlayTrigger", "This trigger gets executed when Play is called.", STriggerSerializeHelper());
-	desc.AddMember(&CTriggerComponent::m_stopTrigger, 'tri2', "stopTrigger", "StopTrigger", "This trigger gets executed when Stop is called.", STriggerSerializeHelper());
+	desc.AddMember(&CTriggerComponent::m_playTrigger, 'play', "play", "Play", "This trigger gets executed when Play is called.", SPlayTriggerSerializeHelper());
+	desc.AddMember(&CTriggerComponent::m_stopTrigger, 'stop', "stop", "Stop", "This trigger gets executed when Stop is called.", SStopTriggerSerializeHelper());
 
 #if defined(INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE)
-	desc.AddMember(&CTriggerComponent::m_playButton, 'btn1', "play", "Play", "Executes the PlayTrigger.", Serialization::FunctorActionButton<std::function<void()>>());
-	desc.AddMember(&CTriggerComponent::m_stopButton, 'btn2', "stop", "Stop", "Stops the PlayTrigger if no StopTrigger assigned otherwise executes the StopTrigger.", Serialization::FunctorActionButton<std::function<void()>>());
+	desc.AddMember(&CTriggerComponent::m_debugInfo, 'dbug', "debug", "Debug", "Debug functionality.", CDebugSerializeHelper());
 #endif  // INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE
 }
 
@@ -87,17 +123,14 @@ void CTriggerComponent::Initialize()
 	}
 
 	// Only play in editor. Launcher is handled via ENTITY_EVENT_START_GAME.
-	if (m_autoPlay && gEnv->IsEditor())
+	if (m_playTrigger.m_autoPlay && gEnv->IsEditor())
 	{
 		Play();
 	}
 
-	DetermineActivityRadius();
-
 #if defined(INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE)
-	m_playButton = Serialization::ActionButton(std::function<void()>([this]() { Play(); }));
-	m_stopButton = Serialization::ActionButton(std::function<void()>([this]() { Stop(); }));
-
+	m_debugInfo.SetComponent(this);
+	m_debugInfo.UpdateDebugInfo();
 	m_previousPlayTriggerId = m_playTrigger.m_id;
 	m_previousStopTriggerId = m_stopTrigger.m_id;
 #endif  // INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE
@@ -109,7 +142,7 @@ Cry::Entity::EventFlags CTriggerComponent::GetEventMask() const
 	Cry::Entity::EventFlags mask = ENTITY_EVENT_AUDIO_TRIGGER_STARTED | ENTITY_EVENT_AUDIO_TRIGGER_ENDED | ENTITY_EVENT_START_GAME | ENTITY_EVENT_DONE;
 
 #if defined(INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE)
-	mask |= ENTITY_EVENT_COMPONENT_PROPERTY_CHANGED | ENTITY_EVENT_RESET;
+	mask |= ENTITY_EVENT_COMPONENT_PROPERTY_CHANGED | ENTITY_EVENT_RESET | ENTITY_EVENT_UPDATE;
 #endif  // INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE
 
 	return mask;
@@ -129,7 +162,7 @@ void CTriggerComponent::ProcessEvent(const SEntityEvent& event)
 			CRY_ASSERT(m_numActiveTriggerInstances > 0);
 			--m_numActiveTriggerInstances;
 
-			if (m_autoPlay && m_numActiveTriggerInstances == 0)
+			if (m_playTrigger.m_autoPlay && m_numActiveTriggerInstances == 0)
 			{
 				CryAudio::SRequestInfo const* const pRequestInfo = reinterpret_cast<CryAudio::SRequestInfo const* const>(event.nParam[0]);
 
@@ -146,13 +179,13 @@ void CTriggerComponent::ProcessEvent(const SEntityEvent& event)
 			break;
 		case ENTITY_EVENT_START_GAME:
 			// Only play in launcher. Editor is handled in Initialize()
-			if (m_autoPlay && !gEnv->IsEditor())
+			if (m_playTrigger.m_autoPlay && !gEnv->IsEditor())
 			{
 				Play();
 			}
 			break;
 		case ENTITY_EVENT_DONE:
-			if (m_autoPlay)
+			if (m_playTrigger.m_autoPlay)
 			{
 				Stop();
 			}
@@ -174,12 +207,10 @@ void CTriggerComponent::ProcessEvent(const SEntityEvent& event)
 						m_pIEntityAudioComponent->StopTrigger(m_previousPlayTriggerId, m_auxObjectId);
 					}
 
-					if (m_autoPlay)
+					if (m_playTrigger.m_autoPlay)
 					{
 						Play();
 					}
-
-					DetermineActivityRadius();
 				}
 				else if (m_previousStopTriggerId != m_stopTrigger.m_id)
 				{
@@ -187,29 +218,31 @@ void CTriggerComponent::ProcessEvent(const SEntityEvent& event)
 					{
 						m_pIEntityAudioComponent->StopTrigger(m_previousStopTriggerId, m_auxObjectId);
 					}
-
-					if (m_autoPlay)
-					{
-						Stop();
-					}
-
-					DetermineActivityRadius();
 				}
 
 				m_previousPlayTriggerId = m_playTrigger.m_id;
 				m_previousStopTriggerId = m_stopTrigger.m_id;
+
+				m_debugInfo.UpdateDebugInfo();
 			}
 			break;
 		case ENTITY_EVENT_RESET:
 			{
-				if (m_autoPlay && (m_numActiveTriggerInstances == 0))
+				if (m_playTrigger.m_autoPlay && (m_numActiveTriggerInstances == 0))
 				{
 					Play();
 				}
-				else if (!m_autoPlay && (m_numActiveTriggerInstances > 0))
+				else if (!m_playTrigger.m_autoPlay && (m_numActiveTriggerInstances > 0))
 				{
 					Stop();
 				}
+
+				m_debugInfo.UpdateDebugInfo();
+			}
+			break;
+		case ENTITY_EVENT_UPDATE:
+			{
+				m_debugInfo.DrawDebugInfo();
 			}
 			break;
 #endif      // INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE
@@ -220,7 +253,7 @@ void CTriggerComponent::ProcessEvent(const SEntityEvent& event)
 //////////////////////////////////////////////////////////////////////////
 void CTriggerComponent::Play()
 {
-	if (m_pIEntityAudioComponent != nullptr && m_playTrigger.m_id != CryAudio::InvalidControlId)
+	if ((m_pIEntityAudioComponent != nullptr) && (m_playTrigger.m_id != CryAudio::InvalidControlId))
 	{
 		CryAudio::SRequestUserData const userData(CryAudio::ERequestFlags::DoneCallbackOnExternalThread | CryAudio::ERequestFlags::CallbackOnExternalOrCallingThread, this);
 		m_pIEntityAudioComponent->ExecuteTrigger(m_playTrigger.m_id, m_auxObjectId, userData);
@@ -230,7 +263,7 @@ void CTriggerComponent::Play()
 //////////////////////////////////////////////////////////////////////////
 void CTriggerComponent::Stop()
 {
-	if (m_pIEntityAudioComponent != nullptr)
+	if ((m_pIEntityAudioComponent != nullptr) && m_stopTrigger.m_canStop)
 	{
 		if (m_stopTrigger.m_id != CryAudio::InvalidControlId)
 		{
@@ -244,30 +277,111 @@ void CTriggerComponent::Stop()
 	}
 }
 
+#if defined(INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE)
 //////////////////////////////////////////////////////////////////////////
-void CTriggerComponent::DetermineActivityRadius()
+inline void ReflectType(Schematyc::CTypeDesc<CDebugSerializeHelper>& desc)
 {
-	CryAudio::STriggerData playTriggerData;
-	CryAudio::STriggerData stopTriggerData;
-
-	if (m_playTrigger.m_id != CryAudio::InvalidControlId)
-	{
-		gEnv->pAudioSystem->GetTriggerData(m_playTrigger.m_id, playTriggerData);
-	}
-
-	if (m_stopTrigger.m_id != CryAudio::InvalidControlId)
-	{
-		gEnv->pAudioSystem->GetTriggerData(m_stopTrigger.m_id, stopTriggerData);
-	}
-
-	m_activityRadius = std::max(playTriggerData.radius, stopTriggerData.radius);
+	desc.SetGUID("585F7020-536A-4FFD-BF62-4F7AF1054ACB"_cry_guid);
 }
 
 //////////////////////////////////////////////////////////////////////////
-void CTriggerComponent::GetActivityRadius(float& radius)
+inline void CDebugSerializeHelper::Serialize(Serialization::IArchive& archive)
 {
-	radius = m_activityRadius;
+	archive(Serialization::ActionButton(functor(*this, &CDebugSerializeHelper::OnPlay)), "play", "Play");
+
+	bool const canStop = (m_pComponent != nullptr) && (m_pComponent->CanStop());
+
+	if (canStop)
+	{
+		archive(Serialization::ActionButton(functor(*this, &CDebugSerializeHelper::OnStop)), "stop", "Stop");
+	}
+
+	archive(m_drawActivityRadius, "drawPlayTriggerRadius", "Draw PlayTrigger Radius");
+
+	if (canStop)
+	{
+		archive(m_drawStopTriggerRadius, "drawStopTriggerRadius", "Draw StopTrigger Radius");
+	}
 }
-} // namespace DefaultComponents
-} // namespace Audio
-} // namespace Cry
+
+//////////////////////////////////////////////////////////////////////////
+void CDebugSerializeHelper::OnPlay()
+{
+	if (m_pComponent != nullptr)
+	{
+		m_pComponent->Play();
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+void CDebugSerializeHelper::OnStop()
+{
+	if (m_pComponent != nullptr)
+	{
+		m_pComponent->Stop();
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+void CDebugSerializeHelper::UpdateDebugInfo()
+{
+	if (m_pComponent != nullptr)
+	{
+		CryAudio::STriggerData triggerData;
+		gEnv->pAudioSystem->GetTriggerData(m_pComponent->GetPlayTriggerId(), triggerData);
+		m_playTriggerRadius = triggerData.radius;
+
+		if (m_pComponent->CanStop())
+		{
+			gEnv->pAudioSystem->GetTriggerData(m_pComponent->GetStopTriggerId(), triggerData);
+			m_stopTriggerRadius = triggerData.radius;
+		}
+		else
+		{
+			m_stopTriggerRadius = 0.0f;
+		}
+	}
+
+	m_canDrawPlayTriggerRadius = (m_drawActivityRadius && (m_playTriggerRadius > 0.0f));
+	m_canDrawStopTriggerRadius = (m_drawStopTriggerRadius && (m_stopTriggerRadius > 0.0f));
+}
+
+//////////////////////////////////////////////////////////////////////////
+void CDebugSerializeHelper::DrawDebugInfo()
+{
+	if (m_pComponent != nullptr)
+	{
+		if (m_canDrawPlayTriggerRadius || m_canDrawStopTriggerRadius)
+		{
+			IRenderAuxGeom* const pAuxGeom = gEnv->pRenderer->GetIRenderAuxGeom();
+
+			if (pAuxGeom != nullptr)
+			{
+				SAuxGeomRenderFlags const currentRenderFlags = pAuxGeom->GetRenderFlags();
+				SAuxGeomRenderFlags newRenderFlags = currentRenderFlags;
+				newRenderFlags.SetCullMode(e_CullModeNone);
+				newRenderFlags.SetFillMode(e_FillModeWireframe);
+				newRenderFlags.SetAlphaBlendMode(e_AlphaBlended);
+				pAuxGeom->SetRenderFlags(newRenderFlags);
+
+				Vec3 const pos = m_pComponent->GetEntity()->GetWorldPos();
+
+				if (m_canDrawPlayTriggerRadius)
+				{
+					pAuxGeom->DrawSphere(pos, m_playTriggerRadius, ColorB(100, 250, 100, 100), false);
+				}
+
+				if (m_canDrawStopTriggerRadius)
+				{
+					pAuxGeom->DrawSphere(pos, m_stopTriggerRadius, ColorB(250, 100, 100, 100), false);
+				}
+
+				pAuxGeom->SetRenderFlags(currentRenderFlags);
+			}
+		}
+	}
+}
+#endif // INCLUDE_DEFAULT_PLUGINS_PRODUCTION_CODE
+}      // namespace DefaultComponents
+}      // namespace Audio
+}      // namespace Cry
