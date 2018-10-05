446762ea7045299d5ab297ef3f4122f7cf733c0b
David Kaye <davidka@crytek.de>
2018-10-05 10:35:42 +0200

    !R (DEV-6377) (CMake) Don't force C standard version for .c files !T (CMake) Clean up Clang compilation settings file. !R (CMake) Clean up PS4 toolchain, and executable and include paths.
    Copied from Perforce
     Change: 1859569

--- Tools/CMake/CRYENGINE-CLANG.cmake
+++ Tools/CMake/CRYENGINE-CLANG.cmake
@@ -22,14 +22,6 @@ set(CLANG_COMMON_FLAGS
 	-Wno-delete-non-virtual-dtor     # Needed to provide virtual dispatch to allow strings to be modified on CryCommon types
 )
 
-if(NOT ANDROID)
-  set(CLANG_COMMON_FLAGS ${CLANG_COMMON_FLAGS} 
-    -msse2
-    -Wunknown-attributes
-    -fno-exceptions
-  )
-endif()
-
 if(ANDROID)
   set(CLANG_COMMON_FLAGS ${CLANG_COMMON_FLAGS}
 	-Wno-deprecated
@@ -38,37 +30,33 @@ if(ANDROID)
     -fms-extensions
 	-D_MSC_EXTENSIONS=1
   )
+else()
+  set(CLANG_COMMON_FLAGS ${CLANG_COMMON_FLAGS} 
+    -msse2
+    -Wunknown-attributes
+    -fno-exceptions
+  )
 endif()
 
 set(CLANG_CPP_COMMON_FLAGS
 	-std=c++11
 )
-
-string(REPLACE ";" " " CLANG_COMMON_FLAGS "${CLANG_COMMON_FLAGS}")
+string(REPLACE ";" " " CLANG_COMMON_FLAGS     "${CLANG_COMMON_FLAGS}")
 string(REPLACE ";" " " CLANG_CPP_COMMON_FLAGS "${CLANG_CPP_COMMON_FLAGS}")
 
-if (NOT (ORBIS AND ${CMAKE_GENERATOR} MATCHES "Visual Studio"))
-	# HACK: Do not apply this to Orbis in Visual Studio; it breaks .c file compilation
-	set(CMAKE_CXX_FLAGS "${CLANG_COMMON_FLAGS} ${CLANG_CPP_COMMON_FLAGS}" CACHE STRING "C++ Common Flags" FORCE)
-else()
-	set(CMAKE_CXX_FLAGS "${CLANG_COMMON_FLAGS} ${CLANG_CPP_COMMON_FLAGS}" CACHE STRING "C++ Common Flags" FORCE)
-endif()
+set(CMAKE_CXX_FLAGS         "${CLANG_COMMON_FLAGS} ${CLANG_CPP_COMMON_FLAGS}" CACHE STRING "C++ Common Flags" FORCE)
+set(CMAKE_CXX_FLAGS_DEBUG   "-g -O0 -D_DEBUG -DDEBUG"  CACHE STRING "C++ Debug Flags"   FORCE)
+set(CMAKE_CXX_FLAGS_PROFILE "-O3 -D_PROFILE -DNDEBUG"  CACHE STRING "C++ Profile Flags" FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -D_RELEASE -DNDEBUG"  CACHE STRING "C++ Release Flags" FORCE)
+set(CMAKE_CXX_STANDARD_LIBRARIES ""                    CACHE STRING "Libraries linked by defalut with all C++ applications." FORCE)
 
-message(STATUS "CMAKE_CXX_FLAGS = ${CMAKE_CXX_FLAGS}" )
-
-# Set MSVC option for Visual Studio properties to be displayed correctly
-set(CMAKE_CXX_FLAGS_DEBUG "-g -O0 -D_DEBUG -DDEBUG" CACHE STRING "C++ Debug Flags" FORCE)
-set(CMAKE_CXX_FLAGS_PROFILE "-O3 -D_PROFILE -DNDEBUG" CACHE STRING "C++ Profile Flags" FORCE)
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -D_RELEASE -DNDEBUG" CACHE STRING "C++ Release Flags" FORCE)
-
-set(CMAKE_CXX_STANDARD_LIBRARIES "" CACHE STRING "Libraries linked by defalut with all C++ applications." FORCE)
-
-set(CMAKE_C_FLAGS "${CLANG_COMMON_FLAGS}" CACHE STRING "C Common Flags" FORCE)
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}" CACHE STRING "C Debug Flags" FORCE)
+set(CMAKE_C_FLAGS         "${CLANG_COMMON_FLAGS}"      CACHE STRING "C Common Flags"  FORCE)
+set(CMAKE_C_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG}"   CACHE STRING "C Debug Flags"   FORCE)
 set(CMAKE_C_FLAGS_PROFILE "${CMAKE_CXX_FLAGS_PROFILE}" CACHE STRING "C Profile Flags" FORCE)
 set(CMAKE_C_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}" CACHE STRING "C Release Flags" FORCE)
 
-#set(CMAKE_LINK_FLAGS "{$CMAKE_LINK_FLAGS}") #-Wl,-dead_strip
+message(STATUS "CMAKE_C_FLAGS = ${CMAKE_C_FLAGS}" )
+message(STATUS "CMAKE_CXX_FLAGS = ${CMAKE_CXX_FLAGS}" )
 
 set(CMAKE_SHARED_LINKER_FLAGS_PROFILE ${CMAKE_SHARED_LINKER_FLAGS_DEBUG} CACHE STRING "Linker Library Profile Flags" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_PROFILE ${CMAKE_EXE_LINKER_FLAGS_DEBUG} CACHE STRING "Linker Executable Profile Flags" FORCE)
