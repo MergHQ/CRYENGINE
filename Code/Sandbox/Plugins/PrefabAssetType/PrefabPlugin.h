9e14e936ebcf0ba707302b8798ceb0a3f699dd9c
Build <buildengineers@crytek.de>
2018-12-19 15:44:22 +0100

    !XB Ensure files are visible via GitFusion.
    Copied from Perforce
     Change: 1902285

---
+++ Code/Sandbox/Plugins/PrefabAssetType/PrefabPlugin.h
@@ -0,0 +1,15 @@
+// Copyright 2001-2018 Crytek GmbH / Crytek Group. All rights reserved.
+
+#pragma once
+#include "IPlugin.h"
+
+class CPrefabPlugin : public IPlugin
+{
+public:
+	CPrefabPlugin() { /* entry point of the plugin, perform initializations */ }
+	~CPrefabPlugin() { /* exit point of the plugin, perform cleanup */ }
+
+	int32       GetPluginVersion() { return 1; }
+	const char* GetPluginName() { return "Prefab Plugin"; }
+	const char* GetPluginDescription() { return "Prefabs are groups of objects that can be placed in the level as instances"; }
+};
