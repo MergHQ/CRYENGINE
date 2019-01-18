cf64a4e475df8d554f8407fe1380351183808ee1
Alexander Klinger <alexanderkl@crytek.de>
2019-01-18 10:41:50 +0100

    !XB (CryDefaultEntities) (CE-19413) Closing the RigidBody Actor component on a entity with a Character Controller will cause a crash
    Copied from Perforce
     Change: 1911332

--- Code/CryPlugins/CryDefaultEntities/Module/DefaultComponents/Physics/CharacterControllerComponent.cpp
+++ Code/CryPlugins/CryDefaultEntities/Module/DefaultComponents/Physics/CharacterControllerComponent.cpp
@@ -102,24 +102,25 @@ void CCharacterControllerComponent::ProcessEvent(const SEntityEvent& event)
 	if (event.event == ENTITY_EVENT_UPDATE)
 	{
 		IPhysicalEntity* pPhysicalEntity = m_pEntity->GetPhysics();
-		CRY_ASSERT_MESSAGE(pPhysicalEntity != nullptr, "Physical entity removed without call to IEntity::UpdateComponentEventMask!");
-
-		// Update stats
-		pe_status_living livingStatus;
-		if (pPhysicalEntity->GetStatus(&livingStatus) != 0)
+		if (pPhysicalEntity)
 		{
-			m_bOnGround = !livingStatus.bFlying;
+			// Update stats
+			pe_status_living livingStatus;
+			if (pPhysicalEntity->GetStatus(&livingStatus) != 0)
+			{
+				m_bOnGround = !livingStatus.bFlying;
 
-			// Store the ground normal in case it is needed
-			// Note that users have to check if we're on ground before using, is considered invalid in air.
-			m_groundNormal = livingStatus.groundSlope;
-		}
+				// Store the ground normal in case it is needed
+				// Note that users have to check if we're on ground before using, is considered invalid in air.
+				m_groundNormal = livingStatus.groundSlope;
+			}
 
-		// Get the player's velocity from physics
-		pe_status_dynamics playerDynamics;
-		if (pPhysicalEntity->GetStatus(&playerDynamics) != 0)
-		{
-			m_velocity = playerDynamics.v;
+			// Get the player's velocity from physics
+			pe_status_dynamics playerDynamics;
+			if (pPhysicalEntity->GetStatus(&playerDynamics) != 0)
+			{
+				m_velocity = playerDynamics.v;
+			}
 		}
 	}
 	else if (event.event == ENTITY_EVENT_COLLISION)
