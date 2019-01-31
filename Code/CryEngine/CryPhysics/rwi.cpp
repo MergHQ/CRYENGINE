e5cd3556e1814a8eb3afb40ef72dfee34c8d63bb
Anton Knyazyev <anton@crytek.de>
2019-01-31 13:18:13 +0100

    !B (Physics) only apply early exit on pierceable hits filled for rwi_max_piercing rays
    Copied from Perforce
     Change: 1917990

--- Code/CryEngine/CryPhysics/rwi.cpp
+++ Code/CryEngine/CryPhysics/rwi.cpp
@@ -377,7 +377,7 @@ gotcontacts:
 						continue;
 					nThroughHitsAux = min(nThroughHitsAux+1, nMaxHits-1-nThroughHits);
 				}
-				if (nThroughHits+1==nMaxHits)	{	// don't look after the last important pierceble hit's cell
+				if (nThroughHits+1==nMaxHits && flags & rwi_max_piercing)	{	// don't look after the last important pierceble hit's cell
 					Vec3 gpt = pgrid->vecToGrid(phits[nThroughHits].pt-pgrid->origin);
 					ilastcell = float2int(gpt.x*entgrid_stepr.x-0.5f) & 0xFFFF | float2int(gpt.y*entgrid_stepr.y-0.5f)<<16;
 				}
