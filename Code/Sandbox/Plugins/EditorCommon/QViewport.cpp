8fb8da73d9d4549773e4e8560c298c34706afb92
Hilko Cords <hilko@crytek.com>
2019-03-18 12:32:18 +0100

    !B (Animation) (CE-19875) Corrected order of submitting and finishing render item lists from tools
    Copied from Perforce
     Change: 1943206

--- Code/Sandbox/Plugins/EditorCommon/QViewport.cpp
+++ Code/Sandbox/Plugins/EditorCommon/QViewport.cpp
@@ -810,6 +810,7 @@ void QViewport::Render(SDisplayContext& context)
 	}
 
 	passInfo.GetIRenderView()->SetShaderRenderingFlags(SHDF_ALLOWHDR | SHDF_SECONDARY_VIEWPORT);
+	// In EF_StartEf(), usage mode is switched to CRenderView::eUsageModeWriting. Thus, render item lists are added below.
 	m_env->pRenderer->EF_StartEf(passInfo);
 
 	SRendParams rp;
@@ -875,6 +876,11 @@ void QViewport::Render(SDisplayContext& context)
 	m_gizmoManager.Display(context);
 
 	m_env->pSystem->GetIPhysicsDebugRenderer()->Flush(m_lastFrameTime);
+
+	for (size_t i = 0; i < m_consumers.size(); ++i)
+		m_consumers[i]->OnViewportRender(rc);
+
+	// In EF_EndEf3D(), usage mode is switched to CRenderView::eUsageModeWritingDone - indicating the end of adding render item lists.
 	m_env->pRenderer->EF_EndEf3D(-1, -1, passInfo);
 
 	if (m_settings->grid.showGrid)
@@ -896,9 +902,6 @@ void QViewport::Render(SDisplayContext& context)
 		DrawOrigin(50, m_height - 50, 20.0f, m_camera->GetMatrix());
 		aux->SetOrthographicProjection(false);
 	}
-
-	for (size_t i = 0; i < m_consumers.size(); ++i)
-		m_consumers[i]->OnViewportRender(rc);
 
 	aux->Submit();
 	aux->SetRenderFlags(oldFlags);
