831c895c3a51d005ed50b16acf75b18dbdc1e81d
Marek Zeman <marekz@crytek.com>
2019-05-17 13:32:52 +0200

    !XB (AISystem, UnitTests) Fixed ProjectPointOnTriangleVertical unit test !XR (AISystem) Changed PointInTriangle function so that it can accept both orienations of triangles.
    Copied from Perforce
     Change: 1973596

--- Code/CryEngine/UnitTests/CryAISystemUnitTest/Navigation/NavMeshTests.cpp
+++ Code/CryEngine/UnitTests/CryAISystemUnitTest/Navigation/NavMeshTests.cpp
@@ -26,39 +26,65 @@ TEST(CryMNMTest, IntersectSegmentSegment)
 
 TEST(CryMNMTest, ProjectPointOnTriangleVertical)
 {
+	const vector3_t upOffset(real_t(0.0f), real_t(0.0f), real_t(2.0f));
+	const vector3_t downOffset(real_t(0.0f), real_t(0.0f), real_t(-2.0f));
+	
+	// Triangle vertices
 	const vector3_t A(real_t(0.0f), real_t(0.0f), real_t(0.0f));
-	const vector3_t B(real_t(0.0f), real_t(1.0f), real_t(0.0f));
-	const vector3_t C(real_t(1.0f), real_t(0.0f), real_t(1.0f));
+	const vector3_t B(real_t(1.0f), real_t(0.0f), real_t(1.0f));
+	const vector3_t C(real_t(0.0f), real_t(1.0f), real_t(0.0f));
+	
+	// Point above A
 	const vector3_t D(real_t(0.0f), real_t(0.0f), real_t(1.0f));
+
+	// Point 'inside' the triangle (when projected to xy-plane)
+	const vector3_t T(real_t(0.5f), real_t(0.1f), real_t(0.0f));
+	
+	// Points in the middle of the triangle edges
+	const vector3_t S1((A + B) * real_t(0.5f));
+	const vector3_t S2((B + C) * real_t(0.5f));
+	const vector3_t S3((C + A) * real_t(0.5f));
+	
 	vector3_t projected;
 
 	// Point above the triangle
-	REQUIRE(Utils::ProjectPointOnTriangleVertical(vector3_t(real_t(0.5f), real_t(0.1f), real_t(2.0f)), A, B, C, projected));
+	REQUIRE(Utils::ProjectPointOnTriangleVertical(T + upOffset, A, B, C, projected));
 	REQUIRE(projected.x == real_t(0.5f));
 	REQUIRE(projected.y == real_t(0.1f));
 	REQUIRE(projected.z == real_t(0.5f));
 
 	// Point below the triangle
-	REQUIRE(Utils::ProjectPointOnTriangleVertical(vector3_t(real_t(0.5f), real_t(0.1f), real_t(-1.0f)), A, B, C, projected));
+	REQUIRE(Utils::ProjectPointOnTriangleVertical(T + downOffset, A, B, C, projected));
 	REQUIRE(projected.x == real_t(0.5f));
 	REQUIRE(projected.y == real_t(0.1f));
 	REQUIRE(projected.z == real_t(0.5f));
 
 	// Point above the triangle's edge
-	REQUIRE(Utils::ProjectPointOnTriangleVertical(vector3_t(real_t(0.5f), real_t(0.0f), real_t(2.0f)), A, B, C, projected));
-	REQUIRE(projected.x == real_t(0.5f));
-	REQUIRE(projected.y == real_t(0.0f));
-	REQUIRE(projected.z == real_t(0.5f));
+	REQUIRE(Utils::ProjectPointOnTriangleVertical(S1 + upOffset, A, B, C, projected));
+	REQUIRE(projected.x == S1.x);
+	REQUIRE(projected.y == S1.y);
+	REQUIRE(projected.z == S1.z);
+
+	REQUIRE(Utils::ProjectPointOnTriangleVertical(S2 + upOffset, A, B, C, projected));
+	REQUIRE(projected.x == S2.x);
+	REQUIRE(projected.y == S2.y);
+	REQUIRE(projected.z == S2.z);
+
+	REQUIRE(Utils::ProjectPointOnTriangleVertical(S3 + upOffset, A, B, C, projected));
+	REQUIRE(projected.x == S3.x);
+	REQUIRE(projected.y == S3.y);
+	REQUIRE(projected.z == S3.z);
 
 	// Point at vertex
-	REQUIRE(Utils::ProjectPointOnTriangleVertical(vector3_t(real_t(0.0f), real_t(0.0f), real_t(0.0f)), A, B, C, projected));
-	REQUIRE(projected.x == real_t(0.0f));
-	REQUIRE(projected.y == real_t(0.0f));
-	REQUIRE(projected.z == real_t(0.0f));
+	REQUIRE(Utils::ProjectPointOnTriangleVertical(A + upOffset, A, B, C, projected));
+	REQUIRE(projected.x == A.x);
+	REQUIRE(projected.y == A.y);
+	REQUIRE(projected.z == A.z);
 
 	// Degenerate triangle tests
-	REQUIRE(!Utils::ProjectPointOnTriangleVertical(vector3_t(real_t(0.0f), real_t(0.5f), real_t(2.0f)), A, B, D, projected));
-	REQUIRE(!Utils::ProjectPointOnTriangleVertical(A, A, A, C, projected));
+	REQUIRE(!Utils::ProjectPointOnTriangleVertical(S3 + upOffset, A, C, D, projected));
+	REQUIRE(!Utils::ProjectPointOnTriangleVertical(A + upOffset, A, A, B, projected));
 	
-	REQUIRE(Utils::ProjectPointOnTriangleVertical(A, A, A, A, projected)); // All points have same z value, z value for projected point can be set
+	REQUIRE(Utils::ProjectPointOnTriangleVertical(A + upOffset, A, A, A, projected)); // All points have same z value, z value for projected point can be set
+	REQUIRE(projected.z == A.z);
 }
