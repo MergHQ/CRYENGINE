dac8f4d2d1081f58edb55c26d0115fcced5bafec
Enrico van Oosten <enrico@crytek.de>
2019-07-03 17:34:17 +0200

    !B (System) Fix sys_spec value override (for consoles) not being set in the cvar, updated sys_spec description.
    Copied from Perforce
     Change: 1994298

--- Code/CryEngine/CrySystem/SystemInit.cpp
+++ Code/CryEngine/CrySystem/SystemInit.cpp
@@ -678,6 +678,7 @@ static void OnSysSpecChange(ICVar* pVar)
 	spec = CONFIG_CUSTOM;
 	GetISystem()->LoadConfiguration("mobile.cfg", 0, eLoadConfigSystemSpec);
 #endif
+	pVar->Set(spec);
 
 	CryLog("OnSysSpecChange(%d)", spec);
 
@@ -5014,7 +5015,7 @@ void CSystem::CreateSystemVars()
 	REGISTER_CVAR2("MemInfo", &profile_meminfo, 0, 0, "Display memory information by modules\n1=on, 0=off");
 
 	m_sys_spec = REGISTER_INT_CB("sys_spec", CONFIG_CUSTOM, VF_ALWAYSONCHANGE,    // starts with CONFIG_CUSTOM so callback is called when setting initial value
-	                             "Tells the system cfg spec. (0=custom, 1=low, 2=med, 3=high, 4=very high, 5=Xbox One, 6=PS4)",
+	                             "Tells the system cfg spec. (0=custom, 1=low, 2=med, 3=high, 4=very high, 5=Xbox One, 6=Xbox One X, 7=PS4)",
 	                             OnSysSpecChange);
 
 	m_sys_SimulateTask = REGISTER_INT("sys_SimulateTask", 0, 0,
