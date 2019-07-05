1a1973ee00c35a2edc5d7784547a1563463f9435
Dmitry Lavrushko <dmitryl@crytek.com>
2019-07-05 14:06:01 +0200

    !B (Sandbox) (CE-20877) Designer objects: removed flickering on creation/modification, correct shadows rendering while
    Copied from Perforce
     Change: 1995780

--- Code/Sandbox/Plugins/CryDesigner/Core/Model.h
+++ Code/Sandbox/Plugins/CryDesigner/Core/Model.h
@@ -569,7 +569,7 @@ public: //! Etc methods
 
 	//! Invalidate AABB referred by nShelf so that the AABB will be updated as needed to have the up-to-date aabb
 	//! \param nShelf the input shelf. any means the both shelves
-	void InvalidateAABB(ShelfID nShelf = eShelf_Any) const;
+	void InvalidateAABB(ShelfID nShelf = eShelf_Any);
 
 	//! Invalidate the whole smoothing groups so that they are updated out of the latest data as needed to have the right renderable mesh from the up-to-date smoothing groups
 	void InvalidateSmoothingGroups() const;
