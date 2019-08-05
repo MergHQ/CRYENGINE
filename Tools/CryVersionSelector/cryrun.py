4555cf8329b970d07a2be89e2fc448ac4f22e272
Enrico van Oosten <enrico@crytek.de>
2019-08-05 14:30:33 +0000

    !I CL 2011372 ce/main -> ce/main_stabilisation !B (CryVersionSelect) (CE-21372) Add warnings for other invalid symbols in installation path
    Copied from Perforce
     Change: 2011541

--- Tools/CryVersionSelector/cryrun.py
+++ Tools/CryVersionSelector/cryrun.py
@@ -387,10 +387,10 @@ def generate_solution(working_directory, cmakelists_dir, config, open_gui):
     # Due to a bug in CMake visual studio generation, we cannot have certain characters in the cmake path.
     # CMake will generate a command for the INSTALL project in visual studio, which reruns cmake on build.
     # However, the path in the command is not properly quoted by CMake which breaks the build.
-    regex = re.compile('[(){}]')
+    regex = re.compile('[{}]'.format(re.escape('[](){}#%^')))
     if "Visual Studio" in generator and os.path.isabs(cmake_path) and regex.search(cmake_path) is not None:
         print("ERROR:")
-        print("Cannot generate a Visual Studio solution for projects outside the engine installation folder when the engine is installed in a path that contains the following characters: '(){}' due to a CMake bug.")
+        print("Cannot generate a Visual Studio solution for projects outside the engine installation folder when the engine is installed in a path that contains the following characters: '[](){}#%^' due to a CMake bug.")
         print("Engine path: {}".format(cmake_path))
         print("Generating solution has failed!")
         sys.exit(1)
