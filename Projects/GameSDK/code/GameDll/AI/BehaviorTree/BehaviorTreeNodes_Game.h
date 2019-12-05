8623d183107320d73b4db425e3b763420ca2c831
Build <buildengineers@crytek.de>
2019-12-05 17:56:20 +0100

    !XT (Git) Synchronizing Git repositories to Perforce

--- Projects/GameSDK/code/GameDll/AI/BehaviorTree/BehaviorTreeNodes_Game.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2001-2019 Crytek GmbH / Crytek Group. All rights reserved.
-
-#pragma once
-
-namespace BehaviorTree { struct INodeFactory; }
-
-void RegisterGameBehaviorTreeNodes();
--- Projects/GameSDK/code/GameDll/HUD_Impl.h
+++
@@ -1,2 +0,0 @@
-// Copyright 2001-2019 Crytek GmbH / Crytek Group. All rights reserved.
-#error No longer in use - make sure you're doing #include "HUD/HUD_Impl.h" instead of #include "HUD_Impl.h"
