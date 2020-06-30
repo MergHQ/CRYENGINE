667fe7e6e30937d0bd32da30452b01be1839dde2
Cry-Build <buildengineers@crytek.com>
2020-06-30 14:56:28 +0200

    Merge remote-tracking branch 'public/release_candidate' into release

--- Code/CryEngine/RenderDll/XRenderD3D9/DX12/API/DX12Resource.cpp
+++ Code/CryEngine/RenderDll/XRenderD3D9/DX12/API/DX12Resource.cpp
@@ -218,6 +218,12 @@ void CResource::DiscardInitialData()
 }
 
 //---------------------------------------------------------------------------------------------------------------------
+int CResource::GetCVarD3D12AsynchronousComputeValue() const
+{
+	return CRenderer::CV_r_D3D12AsynchronousCompute;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
 bool CResource::Init(ID3D12Resource* pResource, D3D12_RESOURCE_STATES eInitialState, const D3D12_RESOURCE_DESC& desc)
 {
 	m_pD3D12Resource = pResource;
